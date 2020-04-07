13def0d24ffc767f841d6b56042d385d6accfeb1
Oliver Johnstone <oliver@loopsoftware.fr>
2020-04-07 18:04:13 +0100

    Initial commit

---
+++ include/path_matcher.h
@@ -0,0 +1,15 @@
+//
+// Created by Oliver Johnstone on 07/04/2020.
+//
+
+#ifndef PATH_MATCHER_PATH_MATCHER_H
+#define PATH_MATCHER_PATH_MATCHER_H
+
+namespace PathMatcher {
+    class Matcher {
+    public:
+        Matcher();
+    };
+}
+
+#endif //PATH_MATCHER_PATH_MATCHER_H
---
+++ src/path_matcher.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Oliver Johnstone on 07/04/2020.
+//
+
+#include <path_matcher.h>
+
+
+PathMatcher::Matcher::Matcher() {
+
+}
